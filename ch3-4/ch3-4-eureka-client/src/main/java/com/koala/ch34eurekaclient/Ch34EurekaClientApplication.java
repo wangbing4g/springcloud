ca3b99a497570be9acea46d3846bbb50d5cb5b22
KoalaBryson <1512212903@qq.com>
2020-03-19 16:47:00 +0800

    ch3-4 启用HTTP

---
+++ ch3-4/ch3-4-eureka-client/src/main/java/com/koala/ch34eurekaclient/Ch34EurekaClientApplication.java
@@ -0,0 +1,15 @@
+package com.koala.ch34eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Ch34EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch34EurekaClientApplication.class, args);
+    }
+
+}
---
+++ ch3-4/ch3-4-eureka-server/src/main/java/com/koala/ch34eurekaserver/Ch34EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.koala.ch34eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Ch34EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch34EurekaServerApplication.class, args);
+    }
+
+}
